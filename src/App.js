34e566e75788d2c113a5e8f1c3d9c61bd4362775
PokizaSdv <pokizasdv@gmail.com>
2023-12-13 03:35:14 -0500

    Create Simple Counter App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            counter: 0
+        };
+    }
+    increment = () => {
+        this.setState((state) => ({
+            counter: state.counter + 1
+        }));
+    };
+    decrement = () => {
+        this.setState((state) => ({
+            counter: state.counter - 1
+        }));
+    };
+
+    render() {
+        return (
+            <main>
+                <button onClick={this.decrement}>-</button>
+                <span>{this.state.counter}</span>
+                <button onClick={this.increment}>+</button>
+            </main>
+        );
+    }
 }
 
 export default App;
